d295f54c76c9456c78b78339a56965aabf0050bc
k <kevinmurphy678@gmail.com>
2018-07-25 22:35:09 -0400

    Add project files.

---
+++ NMSBackup/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace NMSBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(358, 304);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Open Save Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(377, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 11);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NMS Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.Red;
+            this.statusLabel.Location = new System.Drawing.Point(379, 31);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(95, 13);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Not Running";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1200000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(514, 329);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "No Mans Sky Save Backup - For when the shit breaks";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ NMSBackup/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace NMSBackup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string NMS_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\HelloGames\\";
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", @NMS_PATH);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            textBox1.AppendText("NMS Save path detected:" + Environment.NewLine + NMS_PATH + Environment.NewLine);
+        }
+
+        public bool NMS_IS_RUNNING = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(Process.GetProcessesByName("NMS").Length > 0)
+            {
+                statusLabel.ForeColor = Color.Green;
+                statusLabel.Text = "Running";
+                NMS_IS_RUNNING = true;
+            }
+            else
+            {
+                statusLabel.ForeColor = Color.Red;
+                statusLabel.Text = "Stopped";
+                NMS_IS_RUNNING = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (NMS_IS_RUNNING)
+            {
+
+                ZipFile.CreateFromDirectory(@NMS_PATH+"\\NMS", @NMS_PATH + "/" + DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss") +".zip",CompressionLevel.Optimal,false);
+                textBox1.AppendText("Backed up save:" + Environment.NewLine + DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss") + ".zip" + Environment.NewLine);
+            }
+        }
+    }
+}
